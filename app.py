15adae9fdcbf5ae703fa863b07c700594b79e933
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2023-11-09 00:54:53 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import plotly.express as px
 import os
+import time
+import webbrowser
 
 app = Flask(__name__)
 
@@ -61,16 +63,60 @@ def plotly_chart():
 
     return render_template('drift_lens_monitor.html', plotly_chart=plotly_chart_div)
 
-@app.route("/process_selection", methods=["POST"])
-def process_selection():
-    selected_dataset = request.form.get("dataset")
-    selected_model = request.form.get("model")
-    selected_window_size = request.form.get("window_size")
 
-    # You can use the selected_dataset, selected_model, and selected_window_size values as needed.
-    # For example, you can pass them to a different template to display the results.
+@app.route("/run_experiment", methods=["GET", "POST"])
+def run_experiment():
+    selected_dataset = ""
+    selected_model = ""
+    selected_window_size = ""
+    selected_drift_pattern = ""
+    if request.method == "POST":
+        # This block should execute only when the form is submitted via POST
+        selected_dataset = request.form.get("dataset")
+        selected_model = request.form.get("model")
+        selected_window_size = request.form.get("window_size")
+        selected_drift_pattern = request.form.get("drift_pattern")
 
-    return f"Selected Dataset: {selected_dataset}, Selected Model: {selected_model}, Selected Window Size: {selected_window_size}"
+        if selected_drift_pattern == "no_drift":
+            print("no drift")
+        elif selected_drift_pattern == "sudden_drift":
+            print("sudden drift")
+        elif selected_drift_pattern == "incremental_drift":
+            print("incremental drift")
+        if selected_drift_pattern == "periodic_drift":
+            print("periodic drift")
+
+        # JavaScript to open a new window and redirect the current window
+        # JavaScript to open a new window and redirect the current window
+        script = """
+        <script>
+            if (!window.alreadyOpened) {
+                // Open a new window with drift_lens_monitor
+                var newWindow = window.open("/drift_lens_monitor", "_blank");
+                newWindow.focus();
+
+                // Delay the redirection by a few milliseconds to ensure the new window has opened
+                setTimeout(function() {
+                    // Redirect the current window to /run_experiment with query parameters
+                    var dataset = "%s";  // Note the use of placeholders here
+                    var model = "%s";
+                    window.location.href = "/run_experiment?dataset=" + dataset + "&model=" + model;
+                }, 100); // You can adjust the delay in milliseconds if needed
+
+                window.alreadyOpened = true; // Set a flag to indicate that the new window is opened
+            }
+        </script>
+        """ % (selected_dataset, selected_model)
+
+        response = make_response(
+            f"dataset={selected_dataset}, Selected Model: {selected_model}, Selected Window Size: {selected_window_size}, Selected Drift Pattern: {selected_drift_pattern} {script}")
+    else:
+        # This block should execute when you access /run_experiment directly
+        response = make_response(render_template("run_experiment.html", dataset=selected_dataset, model=selected_model, drift_pattern=selected_drift_pattern))
+
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

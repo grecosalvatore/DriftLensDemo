fdeaf5c857e41e0db02d73d471144786b4722bb4
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2023-11-08 14:10:16 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 import plotly.express as px
 import os
 
 app = Flask(__name__)
+
+def get_datasets_models_and_window_sizes():
+    base_directory = "static/use_cases/datasets"
+    datasets = []
+
+    for dataset_dir in os.listdir(base_directory):
+        if os.path.isdir(os.path.join(base_directory, dataset_dir)):
+            models = []
+
+            dataset_path = os.path.join(base_directory, dataset_dir)
+            models_path = os.path.join(dataset_path, "models")
+
+            if os.path.exists(models_path):
+                for model_dir in os.listdir(models_path):
+                    if os.path.isdir(os.path.join(models_path, model_dir)):
+                        window_sizes = []
+
+                        model_path = os.path.join(models_path, model_dir)
+                        window_sizes_path = os.path.join(model_path, "window_sizes")
+
+                        if os.path.exists(window_sizes_path):
+                            window_sizes = [ws for ws in os.listdir(window_sizes_path) if os.path.isdir(os.path.join(window_sizes_path, ws))]
+
+                        models.append({"name": model_dir, "window_sizes": window_sizes})
+
+            datasets.append({"name": dataset_dir, "models": models})
+
+    return datasets
 
 @app.route("/")
 def home():
@@ -13,8 +40,8 @@ def home():
 @app.route("/use_cases")
 def use_cases():
     title = 'DriftLens'
-    available_datasets = get_datasets_and_models()
-    return render_template('use_cases.html', title=title, datasets=available_datasets)
+    available_data = get_datasets_models_and_window_sizes()
+    return render_template('use_cases.html', title=title, data=available_data)
 
 @app.route("/upload")
 def upload():
@@ -34,34 +61,16 @@ def plotly_chart():
 
     return render_template('drift_lens_monitor.html', plotly_chart=plotly_chart_div)
 
-
-
-def get_datasets_and_models():
-    base_directory = "static/use_cases/datasets"
-    datasets = []
-    for dataset_dir in os.listdir(base_directory):
-        if os.path.isdir(os.path.join(base_directory, dataset_dir)):
-            models = []
-            models_path = os.path.join(base_directory, dataset_dir, "models")
-            if os.path.exists(models_path):
-                for model_dir in os.listdir(models_path):
-                    if os.path.isdir(os.path.join(models_path, model_dir)):
-                        models.append(model_dir)
-            datasets.append({"name": dataset_dir, "models": models})
-    return datasets
-
-
-
 @app.route("/process_selection", methods=["POST"])
 def process_selection():
     selected_dataset = request.form.get("dataset")
     selected_model = request.form.get("model")
+    selected_window_size = request.form.get("window_size")
 
-    # You can use the selected_dataset and selected_model values as needed.
+    # You can use the selected_dataset, selected_model, and selected_window_size values as needed.
     # For example, you can pass them to a different template to display the results.
 
-    return f"Selected Dataset: {selected_dataset}, Selected Model: {selected_model}"
-
+    return f"Selected Dataset: {selected_dataset}, Selected Model: {selected_model}, Selected Window Size: {selected_window_size}"
 
 if __name__ == '__main__':
     app.run(debug=True)

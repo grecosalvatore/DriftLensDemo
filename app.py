c42778a0664731a0c8b5d024d3955245cc4ead5e
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2023-11-08 10:28:09 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import plotly.express as px
 
 app = Flask(__name__)
 
@@ -6,3 +7,19 @@ app = Flask(__name__)
 def home():
     title = 'DriftLens'
     return render_template('home.html', title=title)
+
+@app.route("/drift_lens_monitor")
+def plotly_chart():
+    # Create or retrieve data for your Plotly chart
+    data = [1, 2, 3, 4, 5]
+
+    # Create the Plotly chart
+    fig = px.line(x=list(range(1, len(data) + 1)), y=data, title='Your Plotly Chart')
+
+    # Generate HTML for the Plotly chart
+    plotly_chart_div = fig.to_html(full_html=False)
+
+    return render_template('drift_lens_monitor.html', plotly_chart=plotly_chart_div)
+
+if __name__ == '__main__':
+    app.run(debug=True)
